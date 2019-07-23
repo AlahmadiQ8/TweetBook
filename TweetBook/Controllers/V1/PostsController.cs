2522b088aaf515e3d02e2078209aa48f92b4c368
Mohammad <m91.alahmadi@gmail.com>
2019-07-22 23:03:49 -0400

    added api versioning

---
+++ TweetBook/Controllers/V1/PostsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic.CompilerServices;
+using TweetBook.Contracts.V1;
+using TweetBook.Domain;
+
+namespace TweetBook.Controllers.V1
+{
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private List<Post> _posts;
+
+        public PostsController()
+        {
+            _posts = new List<Post>();
+            for (var i = 0; i < 5; i++)
+            {
+                _posts.Add(new Post() { Id = Guid.NewGuid().ToString() });
+            }
+        }
+
+        [HttpGet(ApiRoutes.Posts.GetAll)]
+        public IActionResult GetAll()
+        {
+            return Ok(_posts);
+        }
+    }
+}
